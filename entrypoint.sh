8672050473fff492c13089e8ca1f0605bbf6a934
phaniamt <45986842+phaniamt@users.noreply.github.com>
2019-11-03 20:46:35 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ -z $EMAIL || -z $DOMAIN || -z $SECRET || -z $AWS_ACCESS_KEY_ID || -z $AWS_SECRET_ACCESS_KEY ]]; then
+        echo "EMAIL, DOMAIN, SECERT, AWS_ACCESS_KEY_ID and $AWS_SECRET_ACCESS_KEY env vars required"
+        env
+        exit 1
+fi
+
+echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >> /opt/aws-config
+echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> /opt/aws-config
+
+NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
+kubectl delete secret ${SECRET} || true
+
+kubectl create secret generic ${SECRET}
+
+certbot certonly -d ${DOMAIN} --dns-route53  -m  ${EMAIL} --agree-tos --non-interactive --server https://acme-v02.api.letsencrypt.org/directory
+
+
+CERTPATH=/etc/letsencrypt/live/*
+
+ls $CERTPATH || exit 1
+
+cat /secret-patch-template.json | \
+        sed "s/NAMESPACE/${NAMESPACE}/" | \
+        sed "s/NAME/${SECRET}/" | \
+        sed "s/TLSCERT/$(cat ${CERTPATH}/fullchain.pem | base64 | tr -d '\n')/" | \
+        sed "s/TLSKEY/$(cat ${CERTPATH}/privkey.pem |  base64 | tr -d '\n')/" \
+        > /secret-patch.json
+
+ls /secret-patch.json || exit 1
+
+# update secret
+curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
